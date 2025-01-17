479a3911fc977cc582d540223e1888afd0cc68c8
Bhushan Gyaniram Patil <bhushu2723@gmail.com>
2025-01-17 19:22:39 +0530

    Create Exceptiondemo.java

---
+++ Exceptiondemo.java
@@ -0,0 +1,19 @@
+
+class Main {
+    public static void main(String[] args) {
+        
+        int [] marks = {10,20,30};
+        
+        try{
+            System.out.println(marks[5]);
+        } catch(Exception exception)
+        {
+        //System.out.println("I am bhushan patil :");
+        }
+        System.out.println("the name is hitesh :");
+    }
+}
+
+/*
+the name is hitesh
+*/
