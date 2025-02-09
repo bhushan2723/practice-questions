b18648d17c4a397423ad15e165f40d5beb9c11fc
Bhushan Gyaniram Patil <bhushu2723@gmail.com>
2025-02-09 11:16:34 +0530

    Create detectCycle.java

---
+++ detectCycle.java
@@ -0,0 +1,41 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+
+        if(head==null)
+        {
+            return null;
+        }
+        
+        ListNode fast = head;
+        ListNode slow = head;
+        ListNode ptr = head;
+
+        while(fast!=null && fast.next!=null)
+        {
+            fast = fast.next.next;
+            slow = slow.next;
+
+            if(fast == slow)
+            {
+                while(slow != ptr)
+                {
+                    ptr = ptr.next;
+                    slow = slow.next;
+                }
+                return ptr;
+            }
+        }
+        return null;
+    }
+}
