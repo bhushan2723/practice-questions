b4fbe58c245122e39b9dac92594e85021742c8d2
Bhushan Gyaniram Patil <bhushu2723@gmail.com>
2025-03-23 22:00:01 +0530

    Create nextPermutation.java

---
+++ nextPermutation.java
@@ -0,0 +1,39 @@
+class Solution {
+    public void nextPermutation(int[] nums) {
+        int n = nums.length;
+        int i = n - 2;
+
+        // Step 1: Find the first decreasing element from the end
+        while (i >= 0 && nums[i] >= nums[i + 1]) {
+            i--;
+        }
+
+        // Step 2: If such element is found, swap it with the next larger element
+        if (i >= 0) {
+            int j = n - 1;
+            while (j >= 0 && nums[j] <= nums[i]) {
+                j--;
+            }
+            swap(nums, i, j);
+        }
+
+        // Step 3: Reverse the remaining elements after i
+        reverse(nums, i + 1, n - 1);
+    }
+
+    // Helper function to swap two elements
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    // Helper function to reverse the subarray
+    private void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            swap(nums, start, end);
+            start++;
+            end--;
+        }
+    }
+}
